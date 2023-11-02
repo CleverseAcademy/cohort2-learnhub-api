487e96fcbf713f648c70608b29f9063bb2d2508f
CSM-intaniger <tanakornp@cleversecorp.com>
2023-11-02 21:31:26 +0700

    refactor(content): :recycle: handler refactoring to use mapper function instead

--- src/handlers/content.ts
+++ src/handlers/content.ts
@@ -4,6 +4,7 @@ import { IContentDto, ICreateContentDto } from "../dto/content";
 import { IErrorDto } from "../dto/error";
 import { AuthStatus } from "../middleware/jwt";
 import { IContentRepository } from "../repositories";
+import mapper from "../utils/contentMapper";
 import { getOEmbedInfo } from "../utils/oembed";
 
 export default class ContentHandler implements IContentHandler {
@@ -11,6 +12,7 @@ export default class ContentHandler implements IContentHandler {
   constructor(repo: IContentRepository) {
     this.repo = repo;
   }
+
   create: RequestHandler<
     {},
     IContentDto | IErrorDto,
@@ -21,16 +23,15 @@ export default class ContentHandler implements IContentHandler {
     const { rating, videoUrl, comment } = req.body;
 
     if (rating > 5 || rating < 0)
-      return res.status(400).json({ message: "rating is out of range 0-5" })
-        .send;
+      return res
+        .status(400)
+        .json({ message: "rating is out of range 0-5" })
+        .end();
 
     const { author_name, author_url, thumbnail_url, title } =
       await getOEmbedInfo(videoUrl);
 
-    const {
-      User: { registeredAt, ...userData },
-      ...contentData
-    } = await this.repo.create(res.locals.user.id, {
+    const content = await this.repo.create(res.locals.user.id, {
       rating,
       videoUrl,
       comment,
@@ -40,15 +41,6 @@ export default class ContentHandler implements IContentHandler {
       videoTitle: title,
     });
 
-    return res
-      .status(201)
-      .json({
-        ...contentData,
-        postedBy: {
-          ...userData,
-          registeredAt: registeredAt.toISOString(),
-        },
-      })
-      .end();
+    return res.status(201).json(mapper(content)).end();
   };
 }
