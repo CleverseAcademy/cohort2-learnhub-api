53be8753dc9f7585aff7069998855a8a81d79b6b
CSM-intaniger <tanakornp@cleversecorp.com>
2023-11-04 22:40:41 +0700

    refactor(content): :alien: refactor oembed API integration to be more explicit on error handling
    two seperated dto, data and error dto.

--- src/dto/oembed.ts
+++ src/dto/oembed.ts
@@ -1,17 +1,21 @@
-export interface OEmbedResponseDto {
+export interface OEmbedResponse {
   url: string;
-  error: string;
   thumbnail_height: number;
   version: string;
   width: number;
   thumbnail_width: number;
   provider_name: string;
-  thumbnail_url: string;
-  author_url: string;
   author_name: string;
+  author_url: string;
+  thumbnail_url: string;
   title: string;
-  type: string;
   height: number;
   provider_url: string;
+  type: string;
   html: string;
+}
+
+export interface OEmbedError {
+  url: string;
+  error: string;
 }
